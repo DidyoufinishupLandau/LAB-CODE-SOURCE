1be3f925e6b4122754727c6880efbd92e23134d3
Pan Zhang <86139044+DidyoufinishupLandau@users.noreply.github.com>
2025-02-21 14:20:55 +0100

    Update revised simulate lindblad.py

--- revised simulate lindblad.py
+++ revised simulate lindblad.py
@@ -11,12 +11,23 @@ delta = 100
 t = 3
 g0 = kin = kappa = 2
 gamma = gamma_r = gamma_phi = 0.01
-e, N = 1.602e-19, 5
+
+wr = 4
+t = 1.1
+delta = np.sqrt(wr**2 - 4*t**2)
+kappa = 0.04
+kin = 0.038
+g0 = 0.1
+gamma_r = 0.03
+gamma_phi = 0.06
+gamma = 2.5
+e, N = 1.602e-19, 14
+g = g0 * 2 * t / wr
 
 # Power range to sweep (test values)
-P_values = np.linspace(1, 100, 30)  # From 1 to 10 in 5 steps
-P_values_large = np.linspace(100,1000,20)
-P_values = np.hstack((P_values, P_values_large))
+P_values = np.linspace(0, 0.2, 10) # From 1 to 10 in 5 steps
+#P_values_large = np.linspace(1,50,20)
+#P_values = np.hstack((P_values, P_values_large))
 # System setup (constant for all simulations)
 a = destroy(N)
 rho_up, rho_down = destroy(2), create(2)
@@ -25,24 +36,30 @@ rho_z = rho_up * rho_down - rho_down * rho_up
 print(rho_z)
 
 
+
 # 3-level operators
 def extend(op):
-    return Qobj(np.block([[op.full(), np.zeros((2, 1))], [np.zeros((1, 3))]]))
+    a = np.block([[op.full(), np.zeros((2, 1))], [np.zeros((1, 3))]])
+    a[2,2] = 1
+    return Qobj(a)
 
-def system_hamiltonian(w):
+def system_hamiltonian(w, K=0.0):
     Delta_c = wr - w
     Delta_q = np.sqrt(delta ** 2 + 4 * t ** 2) - w
-    g = g0 * 2 * t / wr
     f = np.sqrt(kin * n_dot)
-    print(f)
+    print('f',f)
+    print('g',g)
+    print('Delta_q',Delta_q)
     #print((tensor(a, rho_up)))
     #print(tensor(a.dag(), rho_down))
     return (Delta_c * tensor(a.dag() * a, qeye(3)) + \
             (Delta_q * tensor(qeye(N), rho_z) / 2) + \
+            K * (tensor(a.dag()*a*a.dag()*a, qeye(3))) + \
             g * (tensor(a, rho_up) + tensor(a.dag(), rho_down)) + \
             f * (tensor(a, qeye(3)) + tensor(a.dag(), qeye(3))))
 
 rho_up = extend(rho_up) # rho +
+print(rho_up)
 rho_down = extend(rho_down) # rho -
 rho_z = extend(rho_z)
 rho_0e = basis(3, 2) * basis(3, 0).dag()
@@ -58,8 +75,8 @@ c_ops = [
 ]
 
 # Common simulation parameters
-tlist = np.linspace(0, 20, 200)
-psi0 = tensor(fock(N, 1), fock(3, 0))
+tlist = np.linspace(0, 30, 300)
+psi0 = tensor(fock(N, 0), fock(3, 1))
 observables = [
     tensor(qeye(N), basis(3, 0) * basis(3, 0).dag()),
     tensor(qeye(N), basis(3, 2) * basis(3, 2).dag())
@@ -79,10 +96,10 @@ for P in P_values:
     gamma_r_out = gamma * (wr + delta) / (wr)
     gamma_r_in = gamma * (wr - delta) / (wr)
     current = e * (output.expect[0] * gamma_r_out -
-                   output.expect[1] * gamma_r_in)
+                   output.expect[1] * gamma_r_in)*10**9*2*np.pi
 
     currents.append(np.trapezoid(current, tlist)/(tlist[-1]-tlist[0]))  # Average steady-state current
-    plt.figure(figsize=(10, 6))
+    #plt.figure(figsize=(10, 6))
     #plt.plot(tlist, current, 'o-')
     #plt.xlabel('Power (P)')
     #plt.ylabel('Time averaged current (A)')
@@ -97,3 +114,7 @@ plt.ylabel('Time averaged current (A)')
 plt.title('Current vs Input Power cavity frequency approximately equal to g, k, gamma')
 plt.grid(True)
 plt.show()
+
+#low drive current:
+up = 1.602*10**-19*16*g**2*kin*gamma
+#low_drive_current =
